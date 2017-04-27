8b19a8a452c59a02fe041697dd9a240ac51c215c
Ali Khalili <ali.khll.1994@gmail.com>
2017-04-27 19:02:01 +0430

    agrgregate root domain model added

---
+++ CqrsPattern/Cqrs/Command/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SimpleCqrs.Domain;
+
+namespace CqrsPattern.Cqrs.Command
+{
+    public class Account : AggregateRoot
+    {
+        public Account(Guid id)
+        {
+            Apply(new AccountCreatedEvent { AggregateRootId = id });
+        }
+
+        public void SetName(string firstName, string lastName)
+        {
+            Apply(new AccountNameSetEvent { FirstName = firstName, LastName = lastName });
+        }
+
+        public void OnAccountCreated(AccountCreatedEvent evt)
+        {
+            Id = evt.AggregateRootId;
+        }
+    }
+}
