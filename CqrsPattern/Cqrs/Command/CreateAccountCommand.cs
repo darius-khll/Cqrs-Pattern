c16a4c6164f9b81b61df965e6ebd9075c30c8ebb
Ali Khalili <ali.khll.1994@gmail.com>
2017-04-29 16:14:03 +0430

    create account command added

---
+++ CqrsPattern/Cqrs/Command/CreateAccountCommand.cs
@@ -0,0 +1,10 @@
+﻿using SimpleCqrs.Commanding;
+
+namespace CqrsPattern.Cqrs.Command
+{
+    public class CreateAccountCommand : ICommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
