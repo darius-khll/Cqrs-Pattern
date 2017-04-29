9185f73de897d23122ba4fdda247b3c14c6a999e
Ali Khalili <ali.khll.1994@gmail.com>
2017-04-29 16:14:15 +0430

    create account command handler added

---
+++ CqrsPattern/Cqrs/Command/CreateAccountCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SimpleCqrs.Commanding;
+using SimpleCqrs.Domain;
+
+namespace CqrsPattern.Cqrs.Command
+{
+    public class CreateAccountCommandHandler : CommandHandler<CreateAccountCommand>
+    {
+        private readonly IDomainRepository repository;
+
+        public CreateAccountCommandHandler(IDomainRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public override void Handle(CreateAccountCommand command)
+        {
+            var account = new Account(Guid.NewGuid());
+            //account.SetName(command.FirstName, command.LastName);
+
+            repository.Save(account);
+        }
+    }
+}
