2bcdd9edadaaef1021acf5e775d5f5707d478e33
Ali Khalili <ali.khll.1994@gmail.com>
2017-04-30 18:47:34 +0430

    account read model

---
+++ CqrsPattern/Cqrs/Query/AccountReadModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CqrsPattern.Cqrs.Query
+{
+    public class AccountReadModel
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+    }
+}
