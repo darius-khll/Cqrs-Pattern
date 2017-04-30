b9a65d106e79fc2f739e22d7498997844f60abf5
Ali Khalili <ali.khll.1994@gmail.com>
2017-04-30 18:47:43 +0430

    account read model service

---
+++ CqrsPattern/Cqrs/Query/AccountReportReadService.cs
@@ -0,0 +1,22 @@
+﻿using CqrsPattern.Cqrs.Command;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CqrsPattern.Cqrs.Query
+{
+    public class AccountReportReadService
+    {
+        private FakeAccountTable fakeAccountDb;
+
+        public AccountReportReadService(FakeAccountTable fakeAccountDb)
+        {
+            this.fakeAccountDb = fakeAccountDb;
+        }
+
+        public IEnumerable<AccountReadModel> GetAccounts()
+        {
+            return from a in fakeAccountDb
+                   select new AccountReadModel { Id = a.Id, Name = a.Name };
+        }
+    }
+}
