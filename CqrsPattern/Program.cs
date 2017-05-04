0ab737a1ad23ca25c319bc31f03319bd8f4d1db8
Ali Khalili <ali.khll.1994@gmail.com>
2017-05-04 14:21:27 +0430

    program file enhanced

--- CqrsPattern/Program.cs
+++ CqrsPattern/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections;
 using SimpleCqrs.Commanding;
 using CqrsPattern.Cqrs.Query;
 using CqrsPattern.Cqrs.Command;
+using CqrsPattern.Cqrs.Db;
 
 namespace CqrsPattern
 {
@@ -14,23 +14,17 @@ namespace CqrsPattern
 
             runtime.Start();
 
-            // Infrastructure and fakes
             var fakeAccountTable = new FakeAccountTable();
-            runtime.ServiceLocator.Register(fakeAccountTable); // Create Fake-db
+            runtime.ServiceLocator.Register(fakeAccountTable);
             runtime.ServiceLocator.Register(new AccountReportReadService(fakeAccountTable));
             var commandBus = runtime.ServiceLocator.Resolve<ICommandBus>();
 
-            // Create and send a couple of command
+            var cmd = new CreateAccountCommand { FirstName = "Ali", LastName = "Kh" };
 
-            var cmdMarcus = new CreateAccountCommand { FirstName = "Marcus", LastName = "Hammarberg" };
-            //var cmdDarren = new CreateAccountCommand { FirstName = "Darren", LastName = "Cauthon" };
-            //var cmdTyrone = new CreateAccountCommand { FirstName = "Tyrone", LastName = "Groves" };
-            commandBus.Send(cmdMarcus);
-            //commandBus.Send(cmdDarren);
-            //commandBus.Send(cmdTyrone);
+            commandBus.Send(cmd);
 
-            // Get the denormalized version of the data back from the read model
             var accountReportReadModel = runtime.ServiceLocator.Resolve<AccountReportReadService>();
+
             Console.WriteLine("Accounts in database");
             Console.WriteLine("####################");
             foreach (var account in accountReportReadModel.GetAccounts())
